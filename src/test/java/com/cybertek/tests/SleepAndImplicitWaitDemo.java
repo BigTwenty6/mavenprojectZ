ff02e9715f2264e9ff339fbbe4a0042908daff6f
Big Z <ziskan1013@gmail.com>
2019-01-22 20:40:51 -0500

    update 1/22/09

--- src/test/java/com/cybertek/tests/SleepAndImplicitWaitDemo.java
+++ src/test/java/com/cybertek/tests/SleepAndImplicitWaitDemo.java
@@ -1,5 +1,60 @@
 package com.cybertek.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class SleepAndImplicitWaitDemo {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        // driver.Driver.getDriver();
+
+    }
+    @AfterMethod
+    public void tearDown()throws InterruptedException{
+        //   Driver.closeDriver();
+    }
+
+    @Test
+    public void testSleep()throws InterruptedException{
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
+        driver.findElement(By.tagName("button")).click();
+        Thread.sleep(10000);
+        System.out.println(driver.findElement(By.id("finish")).getText());
+
+
+    }
+
+    @Test
+    public void implicitWait(){
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
+        driver.findElement(By.tagName("button")).click();
+
+        System.out.println(driver.findElement(By.id("finish")).getText());
+
+    }
+
+    @Test
+    public void implicitWait2() {
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+
+        driver.findElement(By.cssSelector("form#checkbox-example>button")).click();
+
+        System.out.println(driver.findElement(By.id("message")).getText());
+
+        driver.findElement(By.cssSelector("form#checkbox-example>button")).click();
+
+        System.out.println(driver.findElement(By.id("checkbox")).isDisplayed());
+    }
+
 }
-    git remote set-url origin http://github.com/YOU/YOUR_REPO
